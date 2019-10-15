2d71f99d6979bdb33465ab5d9973d7f428e79319
Galih Julianto <gjulianto17@gmail.com>
2019-10-15 13:34:14 +0700

    Update User Schema
    Update User Schema with Profil Subdocument

--- user.js
+++ user.js
@@ -1,6 +1,28 @@
 let mongoose = require('mongoose')
 let validator = require('validator')
 
+let profilSchema = new mongoose.Schema({
+  birthDate: {
+    type: Date,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  }
+  joinDate: {
+    type: Date,
+    required: true
+  },
+  fullname: {
+    type: String,
+    required: true
+  },
+  position: {
+    type: Schema.Types.ObjectId, ref: "Setting"
+  }
+})
+
 let userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -13,7 +35,8 @@ let userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true
-  }
+  },
+  profil: [ profilSchema ]
 })
 
 module.exports = mongoose.model('User', userSchema)
